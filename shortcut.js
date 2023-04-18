bac9cf2bebd094eb5f41a6524230faf1707af696
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-19 03:09:57 +0600

    shortcut added

---
+++ shortcut.js
@@ -0,0 +1,64 @@
+// 'a', 5, true, {}, []
+// '', 0, false, null, undefined
+
+// check truthy
+
+let myVar = 5;
+if (myVar) {
+     myVar = myVar * 100;
+}
+else {
+     myVar = 0;
+}
+
+// check falsy
+
+let myMoney = 50;
+if (!myMoney) {
+     console.log('fokir');
+}
+
+const money = 100;
+let food;
+if (money > 110) {
+     food = 'kacci';
+}
+else {
+     food = 'biscuit';
+}
+
+// ternary
+
+let khana = money > 110 ? 'kacci' : 'biscuit';
+
+let drink = (money > 80 && myVar < 20) ? 'Dew' : 'mineral water';
+// console.log(drink);
+
+//number to string conversion
+
+const num1 = 96;
+const numStr = num1 + '';
+// console.log(numStr);
+
+// string to number
+
+const input = '4578';
+const inputNumber = +input;
+// console.log(inputNumber);
+
+//
+
+let isActive = true;
+const showUser = () => console.log('Load User');
+const hideUser = () => console.log('Hide User');
+
+isActive ? showUser() : hideUser();
+
+// use && if the left side is true then right side will be executed
+isActive && showUser();
+
+// use || if the left side is false then right side will be executed
+isActive || hideUser();
+
+// toggle boolean
+isActive = !isActive; // negation
